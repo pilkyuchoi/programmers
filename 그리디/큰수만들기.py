df717e16eb29855272f97e3d685fd1fd2d3669f4
pilkyuchoi <eat7777@naver.com>
2021-10-31 21:40:09 +0900

    update

--- 그리디/큰수만들기.py
+++ 그리디/큰수만들기.py
@@ -1,3 +1,14 @@
+#순서가 있을 땐 스택활용 고려하기!
+
 def solution(number, k):
-    answer = ''
-    return answer
+    answer = [] 
+    
+    for num in number:
+
+        while k > 0 and answer and answer[-1] < num:
+            answer.pop()
+            k -= 1
+                
+        answer.append(num)
+
+    return ''.join(answer[:len(number)-k])
