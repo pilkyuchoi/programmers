35a0b55f3bba847fc81e1cf8de0d48616da7f817
pilkyuchoi <eat7777@naver.com>
2021-11-03 14:46:34 +0900

    update

--- 동적계획법/N으로표현.py
+++ 동적계획법/N으로표현.py
@@ -1,26 +1,26 @@
+#직접 적어보면서 규칙 찾기
+
 def solution(N, number):
     answer = -1
     dp = []
-    
-    for i in range (1,9) : 
-    	all_case = set()
-        check_number = int(str(N)* i)
-        all_case.add(check_number)
-        
+
+    for i in range(1, 9):
+        numbers = set()
+        numbers.add(int(str(N) * i))
+
         for j in range(0,i-1):
-            for op1 in dp[j]:
-                for op2 in dp[-j-1] :
-                    all_case.add(op1 - op2)
-                    all_case.add(op1 + op2)
-                    all_case.add(op1 * op2)
-                    if op2 != 0:
-                        all_case.add(op1 // op2)
-                        
-        if number in all_case:
+            for x in dp[j]:
+                for y in dp[-j-1]:
+                    numbers.add(x - y)
+                    numbers.add(x + y)
+                    numbers.add(x * y)
+                    if y != 0:
+                        numbers.add(x // y)
+
+        if number in numbers:
             answer = i
             break
-            
-        dp.append(all_case) 
-        
+
+        dp.append(numbers)
 
     return answer
