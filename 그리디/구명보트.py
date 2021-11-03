8aac76298f2c7b4a9c55f1335e84daedce3d88e9
pilkyuchoi <eat7777@naver.com>
2021-11-03 16:05:30 +0900

    update

--- 그리디/구명보트.py
+++ 그리디/구명보트.py
@@ -1,17 +1,21 @@
-people = [70, 50, 80, 50]
-limit = 100
+#if 에서 & 쓰지 말자 . and라고 쓰자
+
+from collections import deque
 
 def solution(people, limit):
+    people = deque(sorted(people, reverse= True))
     answer = 0
-    weight = limit
-    while people:
-        limit = weight
-        boat = 0 
-        while limit > 0  and len(people) > 0 and boat <= 2:
-                limit - people.pop() 
-                answer += 1
-                boat += 1
-        
-    return answer
 
-solution(people, limit)
+    while people:
+        if len(people) == 1:
+            people.pop()
+            answer += 1
+        elif people[0] + people[-1] <= limit:
+            people.popleft()
+            people.pop()
+            answer += 1
+        else:
+            people.popleft()
+            answer += 1
+
+    return answer
