a91640ebb125b101b682f13b3c375d0e499ae7a6
pilkyuchoi <eat7777@naver.com>
2021-11-06 23:04:15 +0900

    update

--- DFS_BFS/단어변환.py
+++ DFS_BFS/단어변환.py
@@ -3,30 +3,6 @@
 begin = 'hit'
 target = 'cog'
 
-words = ["hot", "dot", "dog", "lot", "log", "cog"]
-
-def dfs(words, start):
-    visited = []
-    stack = []
-
-    stack.append(start)
-
-    while stack:
-        node = stack.pop()
-        
-        for i in range(len(words)):
-            if len([j for j in range(len(words[i])) if words[i][j] != node[j]]) == 1:
-                if words[i] not in visited:
-                    visited.append(words[i])
-                    stack.append(words[i])
-
-        return visited
-
 def solution(begin, target, words):
     if target not in words:
         answer = 0
-
-    answer = len(dfs(words, begin)) - 1
-    return answer
-
-dfs(words, begin)
