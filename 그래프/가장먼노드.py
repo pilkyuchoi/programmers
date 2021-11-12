0019cfde8cab97be549138d93fac7996155380c1
pilkyuchoi <eat7777@naver.com>
2021-11-12 19:01:53 +0900

    update

--- 그래프/가장먼노드.py
+++ 그래프/가장먼노드.py
@@ -1,3 +1,7 @@
+# DFS, BFS 그래프 만들 때 딕셔너리 고려
+# 딕셔너리 get 함수 dic.get('key') 하면 그에 해당하는 value를 돌려주고, 
+# 해당 키가 없으면 dic.get('key', value) 하면 key, value를 새로 넣어준다 (이미 있으면 있는 거 돌려줌)
+
 n = 6
 vertex = [[3, 6], [4, 3], [3, 2], [1, 3], [1, 2], [2, 4], [5, 2]]
 
@@ -5,27 +9,25 @@ vertex = [[3, 6], [4, 3], [3, 2], [1, 3], [1, 2], [2, 4], [5, 2]]
 from collections import deque
 
 def solution(n, vertex):
-    graph = []
-    for i in range(n+1):
-        graph.append(set())
-
+    graph = {}
     for ver in vertex:
-        for i in range(1, n+1):
-            if i in ver:
-                for ve in ver:
-                    if ve != i:
-                        graph[i].add(ve)
+        graph[ver[0]] = graph.get(ver[0], []) + [ver[1]]
+        graph[ver[1]] = graph.get(ver[1], []) + [ver[0]]
 
     queue = deque([1])
-    visited = [False] * (n+1)
+    visited = [0] * (n+1)
+    visited[1] = 1
 
     while queue:
-        v = queue.popleft()
-        for i in graph[v]:
-            if not visited[i]:
-                queue.append(i)
-                visited[i] = True
+        length = len(queue)
+        for i in range(length):
+            v = queue.popleft()
+            for i in graph[v]:
+                if not visited[i]:
+                    queue.append(i)
+                    visited[i] = 1
 
-    return 
+
+    return length
 
 solution(n, vertex)
