cce347452458490d6f108bb2459005fa17ac85ec
pilkyuchoi <eat7777@naver.com>
2021-11-12 20:51:56 +0900

    update

--- level1/숫자문자열과영단어.py
+++ level1/숫자문자열과영단어.py
@@ -1,3 +1,14 @@
+# 이거랑은 상관없지만 정규식 외우자
+
+l = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+dic = {}
+for a, i in zip(l, range(10)):
+    dic[a] = str(i)
+
 def solution(s):
-    answer = 0
-    return answer
+    for a in l:
+        if a in s:
+            s = s.replace(a, dic[a])
+    
+    return int(s)
+
