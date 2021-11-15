7a77d3c728a6d21a16ea4bed1da609a92bb56f0f
pilkyuchoi <eat7777@naver.com>
2021-11-15 11:32:26 +0900

    update

--- level1/크레인인형뽑기게임.py
+++ level1/크레인인형뽑기게임.py
@@ -1,7 +1,17 @@
-board = [[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]]
-moves = [1,5,3,5,1,2,1,4]
+# 이중 포문 break는 break 들어있는 해당 포문 하나만 빠져나감
 
 def solution(board, moves):
-    
     answer = 0
+    l = []
+    for m in moves:
+        for b in board:
+            if b[m-1] != 0:
+                l.append(b[m-1])
+                b[m-1] = 0
+                if len(l) >= 2:
+                    if l[-1] == l[-2]:
+                        l = l[:-2]
+                        answer += 2
+                break
+
     return answer
