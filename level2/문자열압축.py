3eac9c924c3b1a4e690da3d0fc9abd7169f1fb76
pilkyuchoi <eat7777@naver.com>
2021-11-15 13:10:06 +0900

    update

---
+++ level2/문자열압축.py
@@ -0,0 +1,26 @@
+# 하나씩 차근차근 만들어서 규칙 찾기!
+
+def solution(s):
+    answer = ''
+    for k in range(1, (len(s) // 2) + 1):
+        result = ''
+        i = 0
+        while i < len(s):
+            cnt = 1
+            j = k
+            while (i+j) < len(s) and s[i:i+k] == s[i+j:i+j+k]:
+                cnt += 1
+                j += k
+            if cnt == 1:
+                result += s[i:i+k]
+            else:
+                result += (str(cnt) + s[i:i+k])
+            i += j
+        if not answer or len(result) < len(answer):
+            answer = result
+
+    if len(s) == 1:
+        answer = s
+
+    return len(answer)
+
