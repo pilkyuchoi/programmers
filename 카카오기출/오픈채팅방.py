6109c922532be970fce764cea07e03ca60ed2101
pilkyuchoi <eat7777@naver.com>
2021-11-16 13:39:47 +0900

    update

--- 카카오기출/오픈채팅방.py
+++ 카카오기출/오픈채팅방.py
@@ -1,2 +1,18 @@
-record = ["Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"]
+# dic['a'] = 1, dic['a'] = 2하면 마지막으로 한 것만 남음
 
+def solution(record):
+    dic = {}
+    for rec in record:
+        words = rec.split()
+        if words[0] in ['Enter', 'Change']:
+            dic[words[1]] = words[2]
+
+    answer = []
+    for rec in record:
+        words = rec.split()
+        if words[0] == 'Enter':
+            answer.append(f'{dic[words[1]]}님이 들어왔습니다')
+        elif words[0] == 'Leave':
+            answer.append(f'{dic[words[1]]}님이 나갔습니다.')
+
+    return answer
