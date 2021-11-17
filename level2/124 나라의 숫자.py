d33dae2ea2ec852a2458ce3d2541037c7c3bf1ca
pilkyuchoi <eat7777@naver.com>
2021-11-17 17:25:00 +0900

    update

---
+++ level2/124 나라의 숫자.py
@@ -0,0 +1,22 @@
+# 2진법, 3진법 공부하자
+
+from itertools import product
+
+onetwofour = [1, 2, 4]
+
+def solution(n):
+    length = 0
+    i = 1
+
+    while length < n:
+        idx = length
+        length += 3**i
+        i += 1
+
+    answer = ''
+    for j in list(product(onetwofour, repeat=(i-1)))[n-idx-1]:
+        answer += str(j)
+
+    return answer
+
+        
