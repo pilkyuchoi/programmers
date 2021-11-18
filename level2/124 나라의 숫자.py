a29bf7b44c407bea014ddd10bfdfb4daf8f672f5
pilkyuchoi <eat7777@naver.com>
2021-11-18 19:07:22 +0900

    update

--- level2/124 나라의 숫자.py
+++ level2/124 나라의 숫자.py
@@ -1,10 +1,13 @@
 # 진법 변환 공부하자
-
-def notation(n, q):
-    rev_based = ''
+def solution(n):
+    result = ''
 
     while n > 0:
-        n, mod = divmod(n, q)
-        rev_based += str(mod)
+        if n % 3:
+            result = str(n%3) + result
+            n //= 3
+        else:
+            result = '4' + result
+            n = n//3 - 1
 
-    return rev_based[::-1]
+    return result
--- level2/진법변환.py
+++ level2/진법변환.py
@@ -1,6 +1,6 @@
 # n진수 -> 10진수
 int('101', 2)
-int('202', 3)
+int('20', 3)
 
 # 10진수 -> 2, 8, 16진수
 bin(16)[2:]
@@ -9,12 +9,21 @@ hex(16)[2:]
 
 #10진수 -> n진수
 def notation(n, q):
-    rev_based = ''
+    result = ''
 
-    while n > 0:
+    while n:
         n, mod = divmod(n, q)
-        rev_based += str(mod)
+        result = str(mod) + result
 
-    return rev_based[::-1]
-    #역순인 진수를 뒤집어 줘야 원래 변환하고자 하는 base가 출력
+    return result
 
+notation(6, 3)
+    
+#재귀함수 이용
+def convert(n, base):
+    T = "0123456789ABCDEF"
+    q, r = divmod(n, base)
+    if q == 0:
+        return T[r]
+    else:
+        return convert(q, base) + T[r]
