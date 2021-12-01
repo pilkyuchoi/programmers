fb2f24e16728f03395801e206daa9306b341ef28
pilkyuchoi <eat7777@naver.com>
2021-12-01 15:51:16 +0900

    update

--- 카카오기출/비밀지도.py
+++ 카카오기출/비밀지도.py
@@ -2,29 +2,24 @@
 # bin(i|j) 하면 i,j를 이진변환하고 같은 자리에 모두 0이 오면 0, 하나라도 1이 있으면 1로 출력
 # '1'.ljust(5,'0') = '10000'
 
-n = 5
-arr1 = [9, 20, 28, 18, 11]
-arr2 = [30, 1, 21, 17, 28]
+def solution(n, arr1, arr2):
+    arr1 = [bin(x)[2:] for x in arr1]
+    arr2 = [bin(x)[2:] for x in arr2]
+    l1 = []
+    l2 = []
+    for a, b in zip(arr1, arr2):
+        while len(a) < n:
+            a = '0' + a
+        while len(b) < n:
+            b = '0' + b
+        l1.append(a)
+        l2.append(b)
+    answer = []
+    for i in range(n):
+        answer.append(''.join([str(int(x)+int(y)) for x, y in zip(l1[i], l2[i])]))
 
-arr1 = [bin(x)[2:] for x in arr1]
-arr2 = [bin(x)[2:] for x in arr2]
-
-l1 = []
-l2 = []
-for a, b in zip(arr1, arr2):
-    while len(a) < n:
-        a = '0' + a
-    while len(b) < n:
-        b = '0' + b
-    l1.append(a)
-    l2.append(b)
-
-answer = []
-for i in range(n):
-    answer.append(''.join([str(int(x)+int(y)) for x, y in zip(l1[i], l2[i])]))
-
-answer = [x.replace('0', ' ') for x in answer]
-answer = [x.replace('1', '#') for x in answer]
-answer = [x.replace('2', '#') for x in answer]
-
-answer
+    answer = [x.replace('0', ' ') for x in answer]
+    answer = [x.replace('1', '#') for x in answer]
+    answer = [x.replace('2', '#') for x in answer]
+    
+    return answer
